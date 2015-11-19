c75eee568a2094f57144799c3bf5d972c594b0f8
Saygun ICYUZ <saygun@users.noreply.github.com>
2015-11-19 14:30:40 +0200

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 git pull
-function doIt() {
+function doIt () {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" -av . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
